79e3c583394c1daebf90bca2c794de0b5a750d56
Dinorá Vicente <dondi8181@gmail.com>
2020-10-27 15:26:09 -0300

    origin

---
+++ JavaScript/1-meuPrimeiroPrograma.js
@@ -0,0 +1,15 @@
+console.log("Meu Primeiro Programa: trabalhando com variáveis");
+
+const idade = 21;
+
+console.log("idade:"); 
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+3;
+console.log(idadeSomada);
+
+//const da nomes para variávies.
+//console.log faz com que apareça na tela quando executado.
---
+++ JavaScript/2-variaveis.js
@@ -0,0 +1,16 @@
+//Console.log("Trabalhando com Variáveis");
+console.log("Trabalhando com Variáveis");
+//JS é case sensitive
+
+//idade = 29;
+//Idade = 39;
+//é possível atribuir uma variável sem colocar o const, mas não é recomendado - 
+//será explicado para frente (escopo global, irá subir de escopo).
+//console.log (idade);
+//console.log (Idade);
+//má pratica.
+
+const idade = 29;
+const nome = "Dinorá";
+let ano = 2020;
+// outro jeito de declarar variáveis.
