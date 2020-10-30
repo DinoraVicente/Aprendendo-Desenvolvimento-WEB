fd621433cca6ba60ec893c6267386fa4f186b30e
Dinorá Vicente <dondi8181@gmail.com>
2020-10-30 12:44:50 -0300

    Add valor de imc p/ todos da tabela

--- JS - WEB/js/principal.js
+++ JS - WEB/js/principal.js
@@ -1,14 +1,39 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+for(var i = 0; i < pacientes.length; i ++ ){
 
-var tdImc = paciente.querySelector(".info-imc");
+    var paciente = pacientes[i];
 
-var imc = peso / (altura * altura);
-tdImc.textContent = imc;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    var pesoValido = true;
+    var alturaValido = true;
+    
+    if(peso <= 0 || peso >= 800){
+        console.log("Peso inválido.");
+        tdImc.textContent = "Peso inválido."; 
+        pesoValido = false;
+    }
+    
+    if(altura <= 0 || altura >= 3){
+        console.log("Altura inválida.");
+        tdImc.textContent = "Altura inválida.";
+        alturaValido = false;
+    }
+    
+    if(alturaValido && pesoValido){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    } else {
+        tdImc.textContent = "Altura e/ou peso inválidos!";
+    }
+}
