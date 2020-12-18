7ce00bed3448b92eb65a09b03b707febe333d767
Dinorá Vicente <dondi8181@gmail.com>
2020-12-18 20:42:11 -0300

    For

---
+++ JS-programadorABordo/loopsFor.js
@@ -0,0 +1,18 @@
+// for (let contador = 0; contador < 10; contador++){
+//     console.log(contador);
+// }
+
+let numerosAleatorios = [3, 8, 10, 31];
+// for(let contador = 0; contador < numerosAleatorios.length; contador++){
+//     console.log(`${contador} - ${numerosAleatorios[contador]}`);
+// }
+
+// for(let i = numerosAleatorios.length - 1; i >= 0; i--){
+//     console.log(`Posição ${i} com decremento: ${numerosAleatorios[i]}`)
+// }
+
+let i=0;
+for( ; i < numerosAleatorios.length;){
+    console.log(`Sem valor inicial e incremento: ${numerosAleatorios[i]}`);
+    i++;
+}
